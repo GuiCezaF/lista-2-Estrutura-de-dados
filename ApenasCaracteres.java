7998f562e0520b83ebf8a0373824963d58c8e3d9
Guilherme Cezarino Felipe <124943256+GuiCezaF@users.noreply.github.com>
2024-03-13 23:51:09 -0300

    Add files via upload

---
+++ ApenasCaracteres.java
@@ -0,0 +1,26 @@
+public class ApenasCaracteres {
+
+  public static boolean isNumero(String str) {
+      if (str.isEmpty()) {
+          return true;
+      }
+      
+      char firstChar = str.charAt(0);
+      if (firstChar < '0' || firstChar > '9') {
+          return false;
+      }
+      
+      return isNumero(str.substring(1));
+  }
+
+  public static void main(String[] args) {
+    
+      String teste1 = "123456"; // Deve retornar true
+      String teste2 = "123456A"; // Deve retornar false
+      String teste3 = "A983B";  // Deve retornar false
+
+      System.out.printf("\n %s eh uma cadeia de strings?: %s \n ", teste1, isNumero(teste1));
+      System.out.printf("\n %s eh uma cadeia de strings?: %s\n", teste2, isNumero(teste2));
+      System.out.printf("\n %s eh uma cadeia de strings?: %s\n ", teste3, isNumero(teste3));
+  }
+}
---
+++ Binaria.java
@@ -0,0 +1,17 @@
+public class Binaria {
+  
+
+  /*
+    Todo número decimal possui também uma representação binária, sendo o seu valor
+  apresentado apenas com os dígitos "0" e "1". Por exemplo, o número 12 em base
+  decimal possui como representação binária o valor 1100 (pois 1 * 23 + 1 * 22 + 0 * 21 +
+  0 * 20). Implemente a função baseBinaria() da classe Binaria.java que
+  recebe como parâmetro um número inteiro decimal positivo e devolve a sua
+  representação binária. Essa função deve ser recursiva.
+   */
+
+
+  public static void main(String[] args) {
+    
+  }
+}
